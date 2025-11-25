1648fc0fed17b856c639716e607b4849fbf44956
mayur-412 <mayurzezariya990@gmail.com>
2025-11-24 23:38:04 -0800

    move
    move

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+       document.addEventListener(
+                "DOMContentLoaded", () => {
+                    new Mmenu( "#menu", {
+                       "offCanvas": {
+                          "position": "right-front"
+                       },
+                       "theme": "light"
+                    });
+                }
+            );
+
+
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
