8e8f73835f47e4602a722b668889a48efe63e6ed
mayur-412 <mayurzezariya990@gmail.com>
2025-12-02 02:10:35 -0800

    add
    add

--- js/custom.js
+++ js/custom.js
@@ -9,21 +9,16 @@
                 }
             );
 
+$('.open').click(function(){
+    $('.popup-sction').fadeIn();
+});
+$('.close').click(function(){
+    $('.popup-sction').fadeOut();
+});
 
-var modal = document.querySelector(".modal");
-var trigger = document.querySelector(".trigger");
-var closeButton = document.querySelector(".close-button");
-
-function toggleModal() {
-    modal.classList.toggle("show-modal");
-}
-
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
-}
-
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
+$('.open-modal').click(function(){
+    $('.popup').fadeIn();
+});
+$('.close').click(function(){
+    $('.popup').fadeOut();
+});
